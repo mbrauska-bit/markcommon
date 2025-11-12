ecf8173979f61858e24a518268600ad615237ccd
Markysha <mbrauska@gmail.com>
2025-11-12 17:20:03 +0100

    .\main.js

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ console.log("CCM!");
 console.log("Mark");
 console.log("lowkkickpoint");
 //Strings//
-console.log("Mark Brauska er født i året " + 2009 + " og er " + 16 + " år gammel!");
-
-
+    console.log("Mark Brauska er født i året " + 2009 + " og er " + 16 + " år gammel!");
     //Oppgaveset2//
      //Variables//
      let alder;             //deklarere variabel//
      alder = 16               //initialisere variabel// 
-        console.log(alder)  
+  console.log(alder)  
      let birthyear=2009 //deklarere and initialisere variabel//
-        console.log(birthyear)
+     let firstname="Mark" //deklratere and initialisere variabel//
+   console.log(birthyear)
+   
         //Const info//
         const yearOfBirth=2009; //deklarere og initialisere konstant//
         console.log(yearOfBirth);
@@ -25,5 +25,16 @@ console.log("Mark Brauska er født i året " + 2009 + " og er " + 16 + " år gam
         const ishockeyplayer=true //deklarere og initialisere konstant//
         console.log(ishockeyplayer);
         const isWorker=false //deklarere og initialisere konstant//
+
         console.log(isWorker);
-        
+    //Datatyper//      
+    console.log(typeof alder); //number//
+    console.log(typeof "16") //string//
+    console.log(typeof birthyear); //number//
+    console.log(typeof "2009") //string//
+    console.log(typeof isStudentfromvgs); //boolean//
+    console.log(typeof "true") //string//
+    console.log(typeof yearOfBirth); //number//
+    console.log(typeof name); //string//
+    console.log(typeof ishockeyplayer); //boolean//
+    console.log(typeof isWorker); //boolean//
