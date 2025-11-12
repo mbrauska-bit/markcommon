d0b85a3741fedd86df60f0a2ff3c80b949255667
Markysha <mbrauska@gmail.com>
2025-11-12 16:41:07 +0100

    .\main.js

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-//Console.log statements//
+//Oppgaveset1//
+ //Console.log statements//
 console.log("Hello, World!");
 console.log("CCM!");
 console.log("Mark");
 console.log("lowkkickpoint");
+//Strings//
+console.log("Mark Brauska er født i året " + 2009 + " og er " + 16 + " år gammel!");
+
+
+    //Oppgaveset2//
+     //Variables//
+     let alder;             //deklarere variabel//
+     alder = 16               //initialisere variabel// 
+        console.log(alder)  
+     let birthyear=2009 //deklarere and initialisere variabel//
+        console.log(birthyear)
+        //Const info//
+        const yearOfBirth=2009; //deklarere og initialisere konstant//
+        console.log(yearOfBirth);
+        const name="Mark" //deklarere og initialisere konstant//
+        console.log(name);
+        const isStudentfromvgs=true //deklarere of intialisere konstant//
+        console.log(isStudentfromvgs);
+        const ishockeyplayer=true //deklarere og initialisere konstant//
+        console.log(ishockeyplayer);
+        const isWorker=false //deklarere og initialisere konstant//
+        console.log(isWorker);
+        
